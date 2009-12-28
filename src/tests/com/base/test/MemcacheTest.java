a52e798eeddff7aab38848e4774fdb370e1d1c82
Victor Igumnov <victori@fabulously40.com>
2009-12-28 11:07:18 -0800

    upgraded to spy-memcache-2.5

--- src/tests/com/base/test/MemcacheTest.java
+++ src/tests/com/base/test/MemcacheTest.java
@@ -1,5 +1,6 @@
 package com.base.test;
 
+import java.io.Serializable;
 import java.util.Arrays;
 
 import org.junit.Test;
@@ -10,8 +11,39 @@ public class MemcacheTest {
 
 	@Test
 	public void test1() {
-		Memcache2 cache = new Memcache2(Arrays.asList("127.0.0.1:11211"), "test");
-		cache.put("hi", "hello world!");
-		System.out.println(cache.get("hi"));
+		final Memcache2 cache = new Memcache2(Arrays.asList("127.0.0.1:11211"), "test");
+		cache.put("hi", new Foo("hi","fooobar"));
+		for(int i = 0 ; i < 10000 ; i++ ) {
+			cache.get("hi");
+		}
+	}
+
+	public static class Foo implements Serializable {
+		private String name;
+		private String body;
+
+		public Foo() {
+		}
+
+		public Foo(final String name, final String body) {
+			super();
+			this.name = name;
+			this.body = body;
+		}
+
+
+
+		public String getName() {
+			return name;
+		}
+		public void setName(final String name) {
+			this.name = name;
+		}
+		public String getBody() {
+			return body;
+		}
+		public void setBody(final String body) {
+			this.body = body;
+		}
 	}
 }
