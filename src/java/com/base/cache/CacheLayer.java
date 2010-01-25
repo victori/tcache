28899a022b0e03772e35eb9ba21d5f8b53984a83
Victor Igumnov <victori@fabulously40.com>
2010-01-24 19:29:24 -0800

    Added async cache invalidation / dogpile protection.

--- src/java/com/base/cache/CacheLayer.java
+++ src/java/com/base/cache/CacheLayer.java
@@ -16,13 +16,22 @@
 
 package com.base.cache;
 
+import java.util.Calendar;
+import java.util.Date;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class CacheLayer {
 	private static transient Logger logger = LoggerFactory.getLogger(CacheLayer.class);
+	private static String DOGPILE_PREFIX = "dp-";
+	private static String DOGPILE_FETCH_PROGRESS_PREFIX = "fdp-";
 
 	public synchronized static Object addOrReplace(final ICache cache, final String key, final IFetch fetch, final int ttl) {
+		return addOrReplace(cache,key,fetch,ttl,0);
+	}
+
+	public synchronized static Object addOrReplace(final ICache cache, final String key, final IFetch fetch, final int ttl,final int dogPileMultiplier) {
 		// if cache is null, just return the result.
 		if (cache == null) {
 			return fetch.getObject();
@@ -30,11 +39,78 @@ public class CacheLayer {
 
 		try {
 			Object o = cache.get(key);
+
 			if (o != null) {
+
+				if(dogPileMultiplier != 0) {
+					logger.debug("Stale cache check.");
+					if(cache.get(DOGPILE_PREFIX+key) == null && cache.get(DOGPILE_FETCH_PROGRESS_PREFIX+key) == null) {
+						// Add 2 minutes ahead for a timeout.
+						Calendar cal = Calendar.getInstance();
+						cal.setTime(new Date());
+						cal.add(Calendar.SECOND, 120);
+
+						cache.put(DOGPILE_FETCH_PROGRESS_PREFIX+key, cal.getTime());
+						logger.debug("Cache stale, fetching new data.");
+						new Thread() {
+							@Override
+							public void run() {
+								Object val = null;
+								if(fetch instanceof IAsyncFetch) {
+									try {
+										val = ((IAsyncFetch)fetch).getObjectAsync();
+									} catch (Exception e) {
+										// sometimes we fail ;-(
+									}
+								} else {
+									val = fetch.getObject();
+								}
+								if(val == null) {
+									logger.debug("Failed to get new data, clearing stale cache.");
+									cache.remove(key);
+									cache.remove(DOGPILE_PREFIX+key);
+									cache.remove(DOGPILE_FETCH_PROGRESS_PREFIX+key);
+								} else {
+									cache.put(key, val, ttl*dogPileMultiplier);
+									cache.put(DOGPILE_PREFIX+key,true,ttl);
+									cache.remove(DOGPILE_FETCH_PROGRESS_PREFIX+key);
+									logger.debug("Cache primed.");
+								}
+							};
+						}.start();
+					} else {
+						logger.debug("Content still valid.");
+					}
+				}
+
+				if(dogPileMultiplier != 0) {
+					if(cache.get(DOGPILE_PREFIX+key) == null) {
+						logger.debug("Returning stale cache.");
+
+						Date timeout = (Date) cache.get(DOGPILE_FETCH_PROGRESS_PREFIX+key);
+						if(timeout != null && new Date().after(timeout)) {
+							logger.debug("Timeout hit, clearing cache.");
+							cache.remove(key);
+							cache.remove(DOGPILE_PREFIX+key);
+							cache.remove(DOGPILE_FETCH_PROGRESS_PREFIX+key);
+						}
+
+					} else {
+						logger.debug("Returning fresh cache.");
+					}
+				}
 				return o;
 			} else {
 				Object val = fetch.getObject();
-				cache.put(key, val, ttl);
+
+				if(dogPileMultiplier != 0) {
+					cache.put(key, val, ttl*dogPileMultiplier);
+					cache.put(DOGPILE_PREFIX+key,true,ttl);
+					cache.remove(DOGPILE_FETCH_PROGRESS_PREFIX+key);
+				} else {
+					cache.put(key, val, ttl);
+				}
+
 				return val;
 			}
 		} catch (Exception e) {
---
+++ src/java/com/base/cache/IAsyncFetch.java
@@ -0,0 +1,5 @@
+package com.base.cache;
+
+public interface IAsyncFetch extends IFetch {
+	public Object getObjectAsync();
+}
